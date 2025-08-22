09b70592128918dcac2ac2b829b7fc09bf86e497
LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>
2025-08-22 17:55:42 +0100

    RDKB-60436 : Cold Standby Support in WAN Manager for WiFi Hotspot Failover.
    Reason for change:
    RDKB-59763 - Develop that US acceptance criteria.
    
    Test Procedure:
    Build should passed
    WANManager functionality should work without any issue
    
    Risks: Medium
    
    Signed-off-by: LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>

--- source/WanManager/wanmgr_net_utils.c
+++ source/WanManager/wanmgr_net_utils.c
@@ -2795,7 +2795,7 @@ int WanManager_Get_IPv6_RA_Configuration(DML_VIRTUAL_IFACE *p_VirtIf, ROUTER_ADV
 
     CcspTraceInfo(("%s %d: Requesting Router solicit for %s \n", __FUNCTION__, __LINE__, p_VirtIf->Name));
 
-    snprintf(cmd, sizeof(cmd), "rdisc6 -1 -r 1 -w 1000 2>/dev/null", p_VirtIf->Name);
+    snprintf(cmd, sizeof(cmd), "rdisc6 -1 -r 1 -w 1000 %s 2>/dev/null", p_VirtIf->Name);
     fp = popen(cmd, "r");
     if (!fp) {
         CcspTraceError(("%s %d: Error Failed to Send Solicit for '%s' interface \n", __FUNCTION__, __LINE__, p_VirtIf->Name));
