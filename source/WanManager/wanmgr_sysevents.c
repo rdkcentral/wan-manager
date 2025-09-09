14f8b889854023016564b9e0892ff6efe13a1074
LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>
2025-09-09 10:35:59 +0100

    RDKB-60436 : Cold Standby Support in WAN Manager for WiFi Hotspot Failover.
    Reason for change:
    RDKB-59763 - Develop that US acceptance criteria.
    
    Test Procedure:
    Build should passed
    WANManager functionality should work without any issue
    
    Risks: Medium
    
    Signed-off-by: LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>

--- source/WanManager/wanmgr_sysevents.c
+++ source/WanManager/wanmgr_sysevents.c
@@ -38,6 +38,7 @@ static token_t sysevent_msg_token;
 #define SYS_IP_ADDR                 "127.0.0.1"
 #define BUFLEN_42                   42
 #define SYSEVENT_IPV6_TOGGLE        "ipv6Toggle"
+#define SYSEVENT_IPV6_ADDR_UPDATE   "ipv6AddressUpdate"
 #define SYSEVENT_VALUE_TRUE        "true"
 #define SYSEVENT_VALUE_FALSE        "false"
 #define SYSEVENT_VALUE_READY        "ready"
@@ -619,6 +620,8 @@ static void *WanManagerSyseventHandler(void *args)
 #endif
 #endif
 
+    async_id_t ipv6_address_change_event_asyncid;
+
     sysevent_set_options(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_IPV6_TOGGLE, TUPLE_FLAG_EVENT);
     sysevent_setnotification(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_IPV6_TOGGLE, &default_route_change_event_asyncid);
 #if defined (_HUB4_PRODUCT_REQ_) || defined(_RDKB_GLOBAL_PRODUCT_REQ_)
@@ -698,6 +701,9 @@ static void *WanManagerSyseventHandler(void *args)
 #endif
 #endif
 
+    sysevent_set_options(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_IPV6_ADDR_UPDATE, TUPLE_FLAG_EVENT);
+    sysevent_setnotification(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_IPV6_ADDR_UPDATE, &ipv6_address_change_event_asyncid);
+
     for(;;)
     {
         char name[BUFLEN_42] = {0};
@@ -761,6 +767,10 @@ static void *WanManagerSyseventHandler(void *args)
                     isDefaultGatewayAdded = 0;
                     CcspTraceWarning(("%s %d Netmonitor Update : IPv6 default route Deleted \n", __FUNCTION__, __LINE__ ));
                 }
+            }
+            else if ( strcmp(name, SYSEVENT_IPV6_ADDR_UPDATE) == 0 )
+            {
+                CcspTraceWarning(("%s %d Netmonitor Update : IPv6 Address Update Event:%s, Value:%s \n", __FUNCTION__, __LINE__, name, val ));
             }
             else if ( strcmp(name, SYSEVENT_ULA_ENABLE) == 0 )
             {
