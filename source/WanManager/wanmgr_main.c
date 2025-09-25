c06ee7259b5c4aa7c66b98f55a57df2980ae5f87
kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>
2025-09-25 11:56:30 +0100

    t2 component name
    Signed-off-by: kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>

--- source/WanManager/wanmgr_main.c
+++ source/WanManager/wanmgr_main.c
@@ -380,7 +380,7 @@ int main(int argc, char* argv[])
 
     WanMgrDmlWanWebConfigInit();
 #ifdef ENABLE_FEATURE_TELEMETRY2_0
-    t2_init(COMPONENT_NAME_WANMANAGER);
+    t2_init("wanmanager");
 #endif
     if ( bRunAsDaemon )
     {
@@ -412,7 +412,7 @@ int main(int argc, char* argv[])
 
     //DATA DELETE
     WanMgr_Data_Delete();
-
+    t2_uninit();
     return 0;
 }
 
