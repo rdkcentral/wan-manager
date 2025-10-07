89d3f5dffa9127dc41a2d95be54276a769a48d0a
Arun R <arun.r@sky.uk>
2025-10-07 14:27:16 +0000

    RDKB-61824: VLAN Discovery Supports Conditional Stop on WAN Detection
    Reason for change:
    To provide vlan discovery control
    
    Test Procedure:
    To enable or disable via psm configuration
    
    Risks: Low
    Signed-off-by: Arun R <arun.r@sky.uk>

--- source/WanManager/wanmgr_interface_sm.c
+++ source/WanManager/wanmgr_interface_sm.c
@@ -1882,21 +1882,21 @@ static eWanState_t wan_transition_start(WanMgr_IfaceSM_Controller_t* pWanIfaceCt
 		// VLAN:DEF instead of the first entry if there is ActiveVlanInUse set that up??? and no load up the Table to Scan
 		// In Here WanMgr_RdkBus_ConfigureVlan if #ifInterfece >1 then associates timer and says configruing
             p_VirtIf->VLAN.ActiveIndex = 0;
-            //DML_VLAN_IFACE_TABLE* pVlanIf = WanMgr_getVirtVlanIfById(p_VirtIf->VLAN.InterfaceList, p_VirtIf->VLAN.ActiveIndex);
+            DML_VLAN_IFACE_TABLE* pVlanIf = WanMgr_getVirtVlanIfById(p_VirtIf->VLAN.InterfaceList, p_VirtIf->VLAN.ActiveIndex);
            // DML_VLAN_IFACE_TABLE* pVlanIf = WanMgr_getVirtVlanIfById(p_VirtIf->VLAN.InterfaceList, p_VirtIf->VLAN.ActiveIndex);
 
 	    // VLAN:DEF instead of the pVlanIf->Interface to be ActiveVlanInUse
-            //strncpy(p_VirtIf->VLAN.VLANInUse, pVlanIf->Interface, sizeof(p_VirtIf->VLAN.VLANInUse));
+            strncpy(p_VirtIf->VLAN.VLANInUse, pVlanIf->Interface, sizeof(p_VirtIf->VLAN.VLANInUse));
 
 	    // VLAN::IJK
-           // strncpy(p_VirtIf->VLAN.ActiveVLANInUse, pVlanIf->Interface, sizeof(p_VirtIf->VLAN.VLANInUse));
-       // CcspTraceInfo(("%s %d - IJK Already  Working ActiveVlanINUse==%s\n", __FUNCTION__, __LINE__, (p_VirtIf->VLAN.ActiveVLANInUse)));
+            strncpy(p_VirtIf->VLAN.ActiveVLANInUse, pVlanIf->Interface, sizeof(p_VirtIf->VLAN.VLANInUse));
+        CcspTraceInfo(("%s %d - IJK Already  Working ActiveVlanINUse==%s\n", __FUNCTION__, __LINE__, (p_VirtIf->VLAN.ActiveVLANInUse)));
 
         }else{
 	//VLAN:IJK
 	//
          //   strncpy(p_VirtIf->VLAN.ActiveVLANInUse, pVlanIf->Interface, sizeof(p_VirtIf->VLAN.VLANInUse));
-       // CcspTraceInfo(("%s %d - IJK Already--NNNNNNNNEEEEEEEE  Working ActiveVlanINUse==%s\n", __FUNCTION__, __LINE__, (p_VirtIf->VLAN.ActiveVLANInUse)));
+        CcspTraceInfo(("%s %d - IJK Already--NNNNNNNNEEEEEEEE  Working ActiveVlanINUse==%s\n", __FUNCTION__, __LINE__, (p_VirtIf->VLAN.ActiveVLANInUse)));
 	
 	}
 	if(strlen(p_VirtIf->VLAN.VLANInUse) <= 0)
@@ -1904,16 +1904,6 @@ static eWanState_t wan_transition_start(WanMgr_IfaceSM_Controller_t* pWanIfaceCt
 	if(strlen(p_VirtIf->VLAN.ActiveVLANInUse) <= 0)
         CcspTraceInfo(("%s %d - IJK Already--NNNNNNNNEEEEEEEE  AAAAAAAWor00000\n", __FUNCTION__, __LINE__));//, (p_VirtIf->VLAN.ActiveVLANInUse)));
 
-	//VLAN:IJK
-            DML_VLAN_IFACE_TABLE* pVlanIf = WanMgr_getVirtVlanIfById(p_VirtIf->VLAN.InterfaceList, p_VirtIf->VLAN.ActiveIndex);
-           strncpy(p_VirtIf->VLAN.ActiveVLANInUse, pVlanIf->Interface, sizeof(p_VirtIf->VLAN.VLANInUse));
-        CcspTraceInfo(("%s %d - IJK Already--NNNNNNNNEEEEEEEE  Working ActiveVlanINUse==%s\n", __FUNCTION__, __LINE__, (p_VirtIf->VLAN.ActiveVLANInUse)));
-
-	if(strlen(p_VirtIf->VLAN.VLANInUse) <= 0)
-        CcspTraceInfo(("%s %d - IJK Already--NNNNNNNNEEEEEEEE  Wor00000ATF\n", __FUNCTION__, __LINE__));//, (p_VirtIf->VLAN.ActiveVLANInUse)));
-	if(strlen(p_VirtIf->VLAN.ActiveVLANInUse) <= 0)
-        CcspTraceInfo(("%s %d - IJK Already--NNNNNNNNEEEEEEEE  AAAAFT  AAAAWor00000\n", __FUNCTION__, __LINE__));//, (p_VirtIf->VLAN.ActiveVLANInUse)));
-											    //
         p_VirtIf->VLAN.Status = WAN_IFACE_LINKSTATUS_CONFIGURING;
 
         //TODO: NEW_DESIGN check for VLAN table
@@ -2242,7 +2232,7 @@ CcspTraceInfo(("ARUN: DISALLWOOOOO - VLANFEnable=%d VLANReset=%d\n",(p_VirtIf->V
 
             //CcspTraceInfo(("%s %d VLAN Discovery . Trying VlanIndex: %d : %s\n", __FUNCTION__, __LINE__,p_VirtIf->VLAN.ActiveIndex, p_VirtIf->VLAN.VLANInUse));
             strncpy(p_VirtIf->VLAN.ActiveVLANInUse, pVlanIf->Interface, (sizeof(p_VirtIf->VLAN.VLANInUse) - 1));
-            CcspTraceInfo(("%s %d IJK:::(usine ActivVLANInUse) VLAN Discovery . Trying VlanIndex: %d : (ActiveVLANInUse=%s)\n", __FUNCTION__, __LINE__,p_VirtIf->VLAN.ActiveIndex, p_VirtIf->VLAN.ActiveVLANInUse));
+            CcspTraceInfo(("%s %d IJK:::(usine ActivVLANInUse) NNNNNNNN VLAN Discovery . Trying VlanIndex: %d : (ActiveVLANInUse=%s)\n", __FUNCTION__, __LINE__,p_VirtIf->VLAN.ActiveIndex, p_VirtIf->VLAN.ActiveVLANInUse));
         }
 
             CcspTraceInfo(("%s %d IJK:::(usine ActivVLANInUse) NNNEEEEE TRYING next (ActiveVLANInUse=%s)\n", __FUNCTION__, __LINE__, p_VirtIf->VLAN.ActiveVLANInUse));
