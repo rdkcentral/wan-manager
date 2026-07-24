36bfc89e555fc9ea434892220fea2d09a62bd98e
JayajothiGopi <Jayajothi_Gopi@comcast.com>
2026-07-24 19:26:03 +0530

    RDKB-64552: DHCP Lease Expire Time IPV4 is not getting updated in GUI (#220)
    Reason for change: DHCP Lease Expire Time IPV4 is not getting updated in GUI
    Test Procedure: Verify the ipv4 dhcp lease time in gui and dmcli cmd output
    Risks: Low
    Priority: P1
    Signed-off-by:Jayajothi_Gopi@comcast.com

--- source/WanManager/wanmgr_sysevents.c
+++ source/WanManager/wanmgr_sysevents.c
@@ -174,6 +174,7 @@ ANSC_STATUS wanmgr_sysevents_ipv4Info_set(const ipc_dhcpv4_data_t* dhcp4Info, co
 
     sysevent_set(sysevent_fd, sysevent_token,SYSEVENT_IPV4_TIME_ZONE, dhcp4Info->timeZone, 0);
 
+#ifndef FEATURE_RDKB_DHCP_MANAGER
     snprintf(name,sizeof(name),SYSEVENT_IPV4_DHCP_SERVER,dhcp4Info->dhcpcInterface);
     sysevent_set(sysevent_fd, sysevent_token,name, dhcp4Info->dhcpServerId,0);
 
@@ -183,6 +184,7 @@ ANSC_STATUS wanmgr_sysevents_ipv4Info_set(const ipc_dhcpv4_data_t* dhcp4Info, co
     snprintf(name,sizeof(name), SYSEVENT_IPV4_LEASE_TIME, dhcp4Info->dhcpcInterface);
     snprintf(value, sizeof(value), "%u",dhcp4Info->leaseTime);
     sysevent_set(sysevent_fd, sysevent_token,name, value, 0);
+#endif
 
     return ANSC_STATUS_SUCCESS;
 }
@@ -275,7 +277,7 @@ if ( TRUE == UseWANMACForManagementServices )
     }
 
     sysevent_set(sysevent_fd, sysevent_token,SYSEVENT_IPV4_TIME_ZONE, dhcp4Info->timeZone, 0);
-
+#ifndef FEATURE_RDKB_DHCP_MANAGER
     snprintf(name,sizeof(name),SYSEVENT_IPV4_DHCP_SERVER,dhcp4Info->ifname);
     sysevent_set(sysevent_fd, sysevent_token,name, dhcp4Info->dhcpServerId,0);
 
@@ -285,7 +287,7 @@ if ( TRUE == UseWANMACForManagementServices )
     snprintf(name,sizeof(name), SYSEVENT_IPV4_LEASE_TIME, dhcp4Info->ifname);
     snprintf(value, sizeof(value), "%u",dhcp4Info->leaseTime);
     sysevent_set(sysevent_fd, sysevent_token,name, value, 0);
-
+#endif
 #endif
     return ANSC_STATUS_SUCCESS;
 }
